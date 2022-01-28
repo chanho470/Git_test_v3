ef60a71afc7478d91c66d7b48a1f5e26df1a9dc6
chltngus129 <chltngus129@gmail.com>
2022-01-28 14:25:34 +0900

    초기 파일

---
+++ src/main/java/org/conan/controller/HelloController.java
@@ -0,0 +1,29 @@
+package org.conan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@RequestMapping("/hello")
+@Log4j
+public class HelloController {
+
+    @GetMapping("/main")
+    public void main() {
+        log.info("hello main");
+    }
+
+    @GetMapping("/world")
+    public void member() {
+        log.info("hello world");
+    }
+
+    @GetMapping("/info")
+    public void info() {
+        log.info("hello info");
+    }
+
+}
